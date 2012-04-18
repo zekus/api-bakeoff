85fa40a4b2b1865382f1c5dbd028256823fbec49
Theodoros Orfanidis <teoulas@gmail.com>
2012-04-18 16:54:15 +0200

    Initial commit for task list (#2)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiBakeoff::Application.config.secret_token = '4650762ff788ac753a68cb3de38fb0ebb0d1b33ff84325721bfb8f321e202c9db1e4252eeba7218154fb8fe3add1143a06df7b43f750e1ee27dd744337705eb8'
